83b972de89c4e2f1d3e3ef8c75f18906736b2f86
Dustin Thomas <stdio@cptlobster.dev>
2023-10-19 12:42:08 -0500

    add error handling for missing files

--- app/controllers/PageController.scala
+++ app/controllers/PageController.scala
@@ -29,6 +29,20 @@ class PageController @Inject()(config: Configuration, val controllerComponents: 
     views.html.main(title, get_links(path))(content)
   }
 
+  def exists(path: String): Boolean = scala.reflect.io.File(path).exists
+
+  def read_file(path: String): String = {
+    val source = scala.io.Source.fromFile("public/" ++ path)
+    try source.mkString finally source.close()
+  }
+
+  def find_or_404(path: String)(callback: () => Result): Result = {
+    if (exists(path)) {
+      callback()
+    } else {
+      NotFound(assemble(path)(Html("<h1>Not found</h1><pre>" ++ path ++ "</pre>")))
+    }
+  }
   /**
    * Create an Action to render an HTML page.
    *
@@ -37,21 +51,23 @@ class PageController @Inject()(config: Configuration, val controllerComponents: 
    * a path of `/`.
    */
   def read_page(path: String) = Action { implicit request: Request[AnyContent] =>
-    val source = scala.io.Source.fromFile("public/" ++ path)
-    val lines = try source.mkString finally source.close()
-    Ok(assemble(path)(Html("<pre>" ++ lines ++ "</pre>")))
+    find_or_404(path)(() => {
+      val lines = read_file(path)
+      Ok(assemble(path)(Html("<pre>" ++ lines ++ "</pre>")))
+    })
   }
 
   def read_md(path: String) = Action { implicit request: Request[AnyContent] =>
-    val source = scala.io.Source.fromFile("public/" ++ path)
-    val lines = try source.mkString finally source.close()
-    val result = Processor.process(lines)
-    Ok(assemble(path)(Html("<main>" ++ result ++ "</main>")))
+    find_or_404(path)(() => {
+      val result = Processor.process(read_file(path))
+      Ok(assemble(path)(Html("<main>" ++ result ++ "</main>")))
+    })
   }
 
   def read_html(path: String) = Action { implicit request: Request[AnyContent] =>
-    val source = scala.io.Source.fromFile("public/" ++ path)
-    val lines = try source.mkString finally source.close()
-    Ok(assemble(path)(Html("<main>" ++ lines ++ "</main>")))
+    find_or_404(path)(() => {
+      val lines = read_file(path)
+      Ok(assemble(path)(Html("<main>" ++ lines ++ "</main>")))
+    })
   }
 }
