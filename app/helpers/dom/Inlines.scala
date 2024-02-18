2b2ee6d13d0d34696865c5e60fac57ab8cb60b65
Dustin Thomas <stdio@cptlobster.dev>
2024-02-17 22:51:06 -0600

    remove parser for now, we'll add that later

--- app/helpers/dom/Inlines.scala
+++
@@ -1,8 +0,0 @@
-package helpers.dom
-
-case class Span(content: List[String | Element], id: String = "", classes: List[String] = List()) extends Element:
-  override val tag: String = "span"
-case class Bold(content: List[String | Element]) extends Element:
-  override val tag: String = "b"
-case class Italic(content: List[String | Element]) extends Element:
-  override val tag: String = "i"
